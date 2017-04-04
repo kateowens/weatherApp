481a1f668ae39869f390339b86595c99ffc034a1
Kate Owens <kowens711@gmail.com>
2017-04-04 16:31:08 -0400

    KO - wrote method to retrieve and parse JSON data from weather underground API, method to set text label to current temperature

--- weatherApp/weatherApp/ViewController.m
+++ weatherApp/weatherApp/ViewController.m
@@ -9,14 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) IBOutlet UILabel *weatherLabel;
+@property (nonatomic, strong) NSString *currentTemperature;
 @end
 
-@implementation ViewController
+//static NSString *const kAPIKey = @"ac7e7e282f1cd5de";
 
+@implementation ViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
@@ -25,5 +26,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)getCurrentTemperature {
+    NSURLSession *URLSession = [NSURLSession sharedSession];
+    NSURL *weatherURL = [NSURL URLWithString:@"http://api.wunderground.com/api/ac7e7e282f1cd5de/conditions/q/MI/Detroit.json"];
+
+    NSURLSessionDataTask *dataTask = [URLSession dataTaskWithURL:weatherURL completionHandler:^(NSData *data,
+                                                                                                NSURLResponse *response,
+                                                                                                NSError *error) {
+        if (error == nil) {
+            NSDictionary *dictionaryFromJSON = [NSJSONSerialization JSONObjectWithData:data
+                                                                               options:0
+                                                                                 error:nil];
+            
+            NSLog(@"%@", [dictionaryFromJSON objectForKey:@"weather"]);
+            
+            self.currentTemperature = [dictionaryFromJSON valueForKey:@"weather"];
+        }
+    }];
+    
+    [dataTask resume];
+}
+
+- (IBAction)showWeather {
+    [self getCurrentTemperature];
+    self.weatherLabel.text = self.currentTemperature;
+}
 
 @end
