7e5b8ba0b4f8c711be8d3788ae1d3f803a1c3d75
Kate Owens <kowens711@gmail.com>
2017-04-13 13:55:46 -0400

    KO - changed string handling

--- weatherApp/weatherApp/ViewController.m
+++ weatherApp/weatherApp/ViewController.m
@@ -10,7 +10,11 @@
 
 @interface ViewController ()
 @property (nonatomic, strong) IBOutlet UILabel *weatherLabel;
+@property (strong, nonatomic) IBOutlet UILabel *minAndMaxTemperatureLabel;
+@property (nonatomic, strong) IBOutlet UILabel *humidityLabel;
+@property (nonatomic, strong) IBOutlet UIImageView *weatherImage;
 @property (nonatomic, strong) NSString *currentTemperature;
+
 @end
 
 @implementation ViewController
@@ -20,7 +24,8 @@
 
 - (void)getCurrentTemperature {
     NSURLSession *URLSession = [NSURLSession sharedSession];
-    NSURL *weatherURL = [NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=\Detroit&APPID=cde859005a79f66e60394e5e2c72546e"];
+
+    NSURL *weatherURL = [NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=Detroit&units=imperial&APPID=cde859005a79f66e60394e5e2c72546e"];
 
     NSURLSessionDataTask *dataTask = [URLSession dataTaskWithURL:weatherURL completionHandler:^(NSData *data,
                                                                                                 NSURLResponse *response,
@@ -31,20 +36,37 @@
                                                                                  error:nil];
             
             NSDictionary *weatherDictionary = [dictionaryFromJSON objectForKey:@"weather"];
-            NSLog(@"%@", [dictionaryFromJSON objectForKey:@"weather"]);
-            NSLog(@"%@", weatherDictionary);
+            NSDictionary *temperatureDictionary = [dictionaryFromJSON objectForKey:@"main"];
             
-            NSString *weatherDescription = [weatherDictionary valueForKey:@"description"];
-            self.weatherLabel.text = weatherDescription;
+            NSArray *weatherDescription = [weatherDictionary valueForKey:@"description"];
+            NSString *temperature = [temperatureDictionary valueForKey:@"temp"];
+            NSString *minTemperature = [temperatureDictionary valueForKey:@"temp_min"];
+            NSString *maxTemperature = [temperatureDictionary valueForKey:@"temp_max"];
+            NSString *humidity = [temperatureDictionary valueForKey:@"humidity"];
+
+            
+            self.weatherLabel.text = [weatherDescription objectAtIndex:0];
+            NSLog(@"%@", temperatureDictionary);
+            NSLog(@"%@", temperature);
+            
+            NSString *minAndMaxTempString = [NSString stringWithFormat: @"Today's low: %@, today's high: %@", (minTemperature), (maxTemperature)];
             
             
-            NSLog(@"%@", self.weatherLabel.text);
+            NSLog(@"%@", minAndMaxTempString);
+            self.minAndMaxTemperatureLabel.text = minAndMaxTempString;
+
             
         }
     }];
     
     [dataTask resume];
 }
+
+- (void)getTenDayForecast {
+    
+}
+
+
 
 - (IBAction)showWeather {
     [self getCurrentTemperature];
